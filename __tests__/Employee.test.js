833665f5ff44cbafdbba6ab8fb561046630c3e6c
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-15 00:35:47 -0500

    Completed 90% on Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,33 @@
-const checkIfEqual = require('../lib/Employee');
+const Employee = require('../lib/Employee');
 
-test('creates an test object', () => {
-    expect(checkIfEqual(10, 10)).toBe(true);
+// self-note employee has 3 arguments in the constructor (name, id, email) and 4 Properties getName(),getID(),getEmail(), getRole()
+
+test('add employee to object', () => {
+    const employee = new Employee('Joseph the Great', 3725, 'JTG55@domain.com');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
 });
+
+test("obtain employee's name", () => {
+    const employee = new Employee('Joseph the Great', 3725, 'JTG55@domain.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test("obtain employee's id", () => {
+    const employee = new Employee('Joseph the Great', 3725, 'JTG55@domain.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test("obtain employee's email", () => {
+    const employee = new Employee('Joseph the Great', 3725, 'JTG55@domain.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+/* test("obtain employee's role", () => {
+    
+})*/
