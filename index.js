21fece95fda9ba1175bbe667c1ae241184358c49
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-09-16 01:39:19 -0500

    added 2nd inquirer prompt for add employees included intern and engineer role with choices of add more team member or not

--- index.js
+++ index.js
@@ -11,48 +11,52 @@ const promptManager = () => {
     {
         type: 'input',
         name: 'name',
-        message: 'What is your manager name of this team? ',
+        message: 'Hello Manager, please enter your name? ',
         validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
-                console.log ('Please enter your manager name!');
+                console.log ('Please enter your name!');
+                return false;
             }
         }
     },
     {
         type: 'input',
         name: 'id',
-        message: "Please enter your manager's ID: ",
+        message: 'Please enter your ID: ',
         validate: idInput => {
             if (idInput) {
                 return true;
             } else {
-                console.log("Please enter your manager's ID! ");
+                console.log('Please enter your ID! ');
+                return false;
             }
         }      
     },
     {
         type: 'input',
         name: 'email',
-        message: "Please enter your manager's email: ",
+        message: 'Please enter your email: ',
         validate: emailInput => {
             if (emailInput) {
                 return true;
             } else {
-                console.log("Please enter your manager's ID! ");
+                console.log('Please enter your ID! ');
+                return false;
             }
         }      
     },
     {
         type: 'input',
         name: 'officeNumber',
-        message: "Please enter your manager's office number: ",
+        message: 'Please enter your office number: ',
         validate: officeInput => {
             if (officeInput) {
                 return true;
             } else {
-                console.log("Please enter your manager's office number! ");
+                console.log('Please enter your office number! ');
+                return false;
             }
         }      
     },
@@ -64,4 +68,96 @@ const promptManager = () => {
     })
 };
 
-promptManager();
+const promptEmployee = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "Enter your employee's role? ",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "Please enter the name of the employee: ",
+            validate: nameInput => {
+                if(nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the name of the employee! ");
+                    return false;
+                }
+            }      
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "Please enter the ID of the employee: ",
+            validate: idInput => {
+                if(idInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the ID of the employee! ");
+                    return false;
+                }
+            }      
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "Please enter the email of the employee: ",
+            validate: emailInput => {
+                if(emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the email of the employee! ");
+                    return false;
+                }
+            }      
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Please enter the GitHub username of the employee: ",
+            when: (input) => input.role === 'Engineer',
+            validate: githubInput => {
+                if(githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the GitHub username of the employee! ");
+                    return false;
+                }
+            }      
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the name of intern's school: ",
+            when: (input) => input.role === 'Intern',
+            validate: schoolInput => {
+                if(schoolInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the name of school! ");
+                    return false;
+                }
+            }      
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddEmployee',
+            message: 'Would you like to enter another team member? ',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        if (employeeData.confirmAddEmployee) {
+            return promptEmployee();
+        } else {
+        console.log(employeeData);
+        }
+    })
+};
+
+
+promptEmployee();
